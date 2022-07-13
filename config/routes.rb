03c3ea5e56a6d0957ff1c04581212c8f571993f8
Ricky G <ricardo_gagliardo@hotmail.com>
2022-07-13 13:06:01 -0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  resources :users
   # Defines the root path route ("/")
   # root "articles#index"
 end
